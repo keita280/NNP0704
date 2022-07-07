4af8c4f6cdd53442a5ba39003df38018724a4abe
keita280 <tanakei1115@gmail.com>
2022-07-08 04:05:35 +0900

    facebook作業途中（JS追加）

--- js/script.js
+++ js/script.js
@@ -57,3 +57,41 @@ $(function(){
 });
 
 
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@ 
+//facebook  のレスポンシブ対応
+//@@@@@@@@@@@@@@@@@@@@@@@@@ 
+jQuery(function($){
+  var first_width = $(window).width();
+   
+  var timer = false;
+  $(window).resize(function() {
+      if (timer !== false) {
+          clearTimeout(timer);
+      }
+      timer = setTimeout(function() {
+          //resize完了時の動作
+          var width = $(window).width();
+          if(width!=first_width){
+   
+              //console.log('resized');
+              fbiframe_reload();
+              first_width=width;
+          }
+      }, 200);
+  });
+   
+  function fbiframe_reload(){//facebookウィジェットの再描画
+   
+          var width = $("#side").width();//親要素のID,class要変更
+   
+          var src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F%25E4%25B8%2580%25E8%2588%25AC%25E7%25A4%25BE%25E5%259B%25A3%25E6%25B3%2595%25E4%25BA%25BA-%25E5%2585%25A8%25E5%259B%25BD%25E7%25B2%25BE%25E7%25A5%259E%25E7%25A7%2591%25E6%25A0%2584%25E9%25A4%258A%25E5%25A3%25AB%25E5%258D%2594%25E4%25BC%259A-106399688763557&tabs&width=680&height=255&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+   
+          $("#fbiframe").attr("src","");       
+          $("#fbiframe").attr("src",src); 
+   
+          $("#fbiframe").attr("width",width); 
+  };
+   
+  fbiframe_reload();
+  });
